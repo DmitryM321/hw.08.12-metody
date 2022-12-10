e6377ad363bff04e0dace0ad8dd2a990cdd69402
Dmitry <a89297175356@gmail.com>
2022-12-10 15:00:27 +0500

    ДЗ методы. Доработано. Попытка 3. я сделал 2 варианта второй задачи. Оба работают. Вопрос правильно ли и какой лучше использовать если да.

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,114 @@
 
 import java.util.Scanner;
 public class Main {
+
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Задача 1"); // Реализуйте метод, который получает в качестве параметра год, проверяет
         System.out.println("Введите год");
-        Scanner a1 = new Scanner(System.in);
-        int year = a1.nextInt();
-        int leapYear = printYear(year);
-        if (leapYear == 1) {
+        int year = scanner.nextInt();
+        int year1 = printYear(year);
+        if (year1 == 1) {
             System.out.println(year + " год — високосный год");
         } else {
             System.out.println(year + " год — невисокосный год");
         }
-        System.out.println("Задача 2"); // Напишите метод, куда подаются два параметра: тип операционной системы (0 — iOS, 1 — Android )
-        System.out.println("Ввведите вашу ОС 1/0");
-        Scanner a2 = new Scanner(System.in);
-        int system = a2.nextInt();
-        System.out.println("Ввведите годы выпуска вашего телефона");
-        Scanner a3 = new Scanner(System.in);
-        int yearSystem = a2.nextInt();
-        int currentYear = yearChoose(yearSystem, system);
 
-        if (currentYear == 1) {
-            System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else if (currentYear == 2) {
+        System.out.println("Задача 2 Вариант 1"); // Задача 2 Вариант 1
+        int versionOS = choseOSAndVersion();
+        if (versionOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке.");
-        } else if (currentYear == 3) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (currentYear == 4) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (versionOS == 2) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if (versionOS == 3) {
+            System.out.println("Установите облегченную  версию приложения для Android по ссылке.");
+        } else if (versionOS == 4) {
+            System.out.println("Установите облегченную  версию приложения для iOS по ссылке.");
         }
 
+        System.out.println("Задача 2. Вариант 1"); // Задача 2 Вариант 2
+        int versionOS1 = choseOSAndVersion1();
+
         System.out.println("Задача 3"); // Доставка карт
-        int deliveryDistance = 95;
-        int Distance = cardDelivery(deliveryDistance);
-        int dayDelivery = 1;
-        if (Distance == 1) {
-            System.out.println("Потребуется дней: " + dayDelivery);
-        } else if (Distance == 2) {
-            System.out.println("Потребуется дней: " + (dayDelivery + 1));
-        } else if (Distance == 3) {
-            System.out.println("Потребуется дней: " + (dayDelivery + 2));
-        } else if (Distance == 4) {
+        int days = deliveryDay();
+        if (days <= 4) {
+            System.out.println("Потребуется дней: " + days);
+        } else {
             System.out.println("Доставки нет");
         }
     }
-    public static int printYear(int year){
-            if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
-                return 1;
-            } else {
-                return 0;
-            }
-        }
 
-    public static int yearChoose ( int yearSystem, int system) {
-        if ((yearSystem >= 2015) && (system == 1)) {
-            return 1;
-        } else if ((yearSystem >= 2015) && (system == 0)) {
-            return 2;
-        } else if ((yearSystem < 2015) && (system == 1)) {
-            return 3;
-        } else if ((yearSystem < 2015) && (system == 0)) {
-            return 4;
+    public static int printYear(int year) { // Метод к Задаче 1
+        int year1 = 0;
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
+            year1 = 1;
         } else {
-            return 5;
+            year1 = 0;
         }
+        return year1;
     }
 
-    public static int cardDelivery ( int deliveryDistance){
-                if (deliveryDistance < 20) {
-                    return 1;
-                } else if (20 <= deliveryDistance && deliveryDistance <= 60) {
-                    return 2;
-                } else if (60 < deliveryDistance && deliveryDistance <= 100) {
-                    return 3;
-                } else {
-                    return 4;
-                }
-            }
+    public static int choseOSAndVersion() {  // Метод к задаче 2 Вариант 1
+        System.out.println("Введите Вашу ОС 0 или 1");
+        int versionOS = scanner.nextInt();
+        System.out.println("Введите год выпуска телефона");
+        int yearTelefone = scanner.nextInt();
+        if ((yearTelefone >= 2015) && (versionOS == 1)) {
+            return 1;
+        } else if ((yearTelefone >= 2015) && (versionOS == 0)) {
+            return 2;
+        } else if ((yearTelefone < 2015) && (versionOS == 1)) {
+            return 3;
+        } else if ((yearTelefone < 2015) && (versionOS == 0)) {
+            return 4;
         }
+        return 0;
+    }
+
+    public static int choseOSAndVersion1() { // Метод к задаче 2 Вариант 1
+        System.out.println("Введите Вашу ОС 0 или 1");
+        int versionOS1 = scanner.nextInt();
+        System.out.println("Введите год выпуска телефона");
+        int yearTelefone1 = scanner.nextInt();
+        if ((yearTelefone1 >= 2015) && (versionOS1 == 1)) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        } else if ((yearTelefone1 >= 2015) && (versionOS1 == 0)) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else if ((yearTelefone1 < 2015) && (versionOS1 == 1)) {
+            System.out.println("Установите облегченную  версию приложения для Android по ссылке.");
+        } else if ((yearTelefone1 < 2015) && (versionOS1 == 0)) {
+            System.out.println("Установите облегченную  версию приложения для iOS по ссылке.");
+        } else {
+        }
+        return versionOS1;
+    }
+
+    public static int deliveryDay() {         // Метод Задача 3
+        int deliveryDistance = 95;
+        int days = 0;
+        if (deliveryDistance < 20) {
+            days = 1;
+        } else if (20 <= deliveryDistance && deliveryDistance <= 60) {
+            days = 2;
+        } else if (60 < deliveryDistance && deliveryDistance <= 100) {
+            days = 3;
+            ;
+        } else if (60 < deliveryDistance && deliveryDistance <= 100) {
+            days = 4;
+        } else {
+            days = 6;
+        }
+        return days;
+    }
+}
+
+
+
+
+
+
+
+
+
+
